967a723fd2fd5b7b279a829d2fe62a5634ad458c
CHRISTIAN A. GALLEGOS YAGUACHI <christian.gallegos@inventar-tech.com>
2026-07-24 17:17:46 -0500

    Contexto y dbsets agregados en persistence

---
+++ src/Starbucks.Persistence/StarbucksDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Starbucks.Domain;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Starbucks.Persistence
+{
+    public class StarbucksDbContext : DbContext
+    {
+        public StarbucksDbContext(DbContextOptions<StarbucksDbContext> options) : base(options)
+        {
+            
+        }
+
+        public required DbSet<Category> Categories { get; set; }
+
+        public required DbSet<Coffe> Coffes { get; set; }
+
+        public required DbSet<Ingredient> Ingredients { get; set; }
+    }
+}
